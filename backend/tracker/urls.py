1a10607b571a0a09f41d72f9b6909a6182512d24
Bleyder <bleydermartinez.dev@gmail.com>
2026-09-06 18:56:11 -0500

    funcionalidad: implementar endpoint de resumen

--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MealListCreateView, TripListCreateView
+from .views import MealListCreateView, TripListCreateView, DashboardView
 
 urlpatterns = [
     path('meals/', MealListCreateView.as_view(), name = 'meals'),
     path('trips/', TripListCreateView.as_view(), name='trips'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .models import MealEntry, TripEntry
 from .serializers import MealEntrySerializer, TripEntrySerializer
+from datetime import timedelta
+from django.utils import timezone
+from django.db.models import Sum
 
 
 #vista para las comidas
@@ -25,4 +29,60 @@ class TripListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, seiralizer):
         seiralizer.save(user=self.request.user)
-    
+
+#vista para calculos de registros de los ultimos 7 dias
+class DashboardView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        today = timezone.localdate()
+        start_date = today - timedelta(days = 6)
+
+        meals = MealEntry.objects.filter(
+            user=request.user,
+            created_at__date__range=[start_date, today]
+        )
+
+        trips = TripEntry.objects.filter(
+            user=request.user,
+            date__range=[start_date, today]
+        )
+
+
+        food_total = meals.aggregate(
+            total=Sum('co2_impact')
+        )['total'] or 0
+
+        travel_total = trips.aggregate(
+            total=Sum('co2_impact')
+        )['total'] or 0
+
+        total_impact = food_total + travel_total
+
+        food_records = [
+            {
+                'id': meal.id,
+                'co2': meal.co2_impact,
+                'createdAt': meal.created_at,
+                'type': 'food'
+            }
+            for meal in meals
+        ]
+
+        travel_records = [
+            {
+                'id': trip.id,
+                'co2': trip.co2_impact,
+                'createdAt': trip.date,
+                'type': 'travel'
+            }
+            for trip in trips
+        ]
+
+        return Response({
+            'foodTotal': food_total,
+            'travelTotal': travel_total,
+            'totalImpact': total_impact,
+            'foodRecords': food_records,
+            'travelRecords': travel_records,
+        })
